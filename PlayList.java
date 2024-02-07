392ef93604fff588af968e0ff4342faece35b7f0
sapirerlich <sapir.erlich@post.runi.ac.il>
2024-02-07 17:36:42 +0200

    hw8

--- PlayList.java
+++ PlayList.java
@@ -108,7 +108,7 @@ class PlayList {
      *  If the list is empty, or the given index is negative or too big for this list, 
      *  does nothing and returns -1. */
     public void remove(int i) {
-        if (i>size){
+        if (i<=size){
             for (int j=i; j<size-1;j++){
                 tracks[j]=tracks[j+1];
             }
